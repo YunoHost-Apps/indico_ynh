7b1f32c7d7ead6fc81bc531a459cb622480c9fde
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2024-11-17 16:13:26 +0100

    Redis_lock unlock

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -125,3 +125,20 @@ else
     install_python $py_version
 fi
 }
+
+############################################
+# Function to lock a Redis database by setting a dummy key
+############################################
+
+redis_lock() {
+    local db=$1
+    redis-cli -n "$db" SET "ynh_lock" "locked" > /dev/null
+}
+############################################
+# Function to unlock a Redis database by deleting the dummy key
+############################################
+
+redis_unlock() {
+    local db=$1
+    redis-cli -n "$db" DEL "ynh_lock" > /dev/null
+}
